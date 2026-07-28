54556b28b0ae8cf24c77bd6705a5796d50d9a731
AxelBossMan <axwoxen@gmail.com>
2026-07-28 17:04:07 +1200

    blurr

--- js/application/kategori-bilder.js
+++ js/application/kategori-bilder.js
@@ -2,13 +2,13 @@
    Stier er relative til dokument-roten (index.html). */
 
 export const KATEGORI_BILDER = Object.freeze({
-  musikk: 'bilder/kategorier/musikk.svg',
-  mat:    'bilder/kategorier/mat.svg',
-  klubb:  'bilder/kategorier/klubb.svg',
-  pafunn: 'bilder/kategorier/pafunn.svg',
+  musikk: 'bilder/kategorier/musikk.jpg',
+  mat:    'bilder/kategorier/mat.jpg',
+  klubb:  'bilder/kategorier/klubb.jpg',
+  pafunn: 'bilder/kategorier/pafunn.jpg',
 });
 
-const GENERELL_FALLBACK = 'bilder/kategorier/musikk.svg';
+const GENERELL_FALLBACK = 'bilder/kategorier/musikk.jpg';
 
 /**
  * Returnerer bilde-URL for et event:
